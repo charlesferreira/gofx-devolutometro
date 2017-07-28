a56f114b8a014ac69b5a662a3bb64c188028a6ef
Charles Ferreira <charles.dev@live.com>
2017-07-27 22:03:42 -0300

    Prepara casa dos bilhões e variações no incremento.

--- get-current-value.php
+++ get-current-value.php
@@ -1,7 +1,9 @@
 <?php
-$value = 0.0;
+header('Content-Type: application/json');
+$value = 999999900; $increment = 19.05;
 if ($content = @file_get_contents('http://www.notaparana.pr.gov.br/')) {
-    preg_match('/creditoCounter[^:]*:([^,]*)/s', $content, $matches);
-    $value = (float)(count($matches > 0) ? $matches[1] : 0);
+    preg_match('/creditoCounter[^:]*value:([^,]*)[^:]*inc:([^,]*)/s', $content, $matches);
+    $value = count($matches > 0) ? (float)$matches[1] : 0;
+    $increment = count($matches > 1) ? (float)$matches[2] : 0;
 }
-die(json_encode($value));
+die(json_encode(compact('value', 'increment')));
--- js/devolutometro.js
+++ js/devolutometro.js
@@ -1,52 +1,61 @@
 $(function() {
     var numbers = {};
-    var value = 0.0;
-    var defaultIncrement = 9.05;
-    var maximumIncrement = defaultIncrement * 1.5;
-    var mininumIncrement = defaultIncrement * 0.2;
+    var virgula;
+    var currentValue;
+    var defaultIncrement;
+    var maximumIncrement = function() { return defaultIncrement * 1.5 };
+    var mininumIncrement = function() { return defaultIncrement * 0.5 };
     var increment = 0;
     var adjustInterval = 60000;
+    var numDigits = 12;
 
     function setup(data) {
-        value = parseFloat(data);
+        currentValue = parseFloat(data.value);
+        defaultIncrement = parseFloat(data.increment);
         setIncrement(defaultIncrement);
-        for (var i = 0; i<= 10; i++)
+        for (var i = 0; i<= numDigits; i++)
             numbers[i] = $('#n' + i);
+        virgula = $('#virgula');
     }
 
     function update() {
-        value += increment;
-        var digits = value.toFixed(2).replace(/\D/g, '');
+        currentValue += increment;
+        var digits = currentValue.toFixed(2).replace(/\D/g, '');
         for (var i in numbers) {
             var digit = parseInt(digits[digits.length - 1 - i]);
             numbers[i].css({
-                backgroundPosition: digit * (-35) + 'px 0'
+                backgroundPosition: digit * (-35) + 'px 0',
+                display: digits.length > i ? 'block' : 'none'
             })
         }
+
+        virgula.css({ display: currentValue > 1000000000 ? 'block' : 'none' });
     }
 
     function setIncrement(newValue) {
-        increment = Math.max(mininumIncrement, newValue > 0
-            ? Math.min(maximumIncrement, newValue)
-            : Math.min(defaultIncrement, increment * 0.75));
+        increment = Math.max(mininumIncrement(), newValue > 0
+            ? Math.min(maximumIncrement(), newValue)
+            : Math.min(defaultIncrement, increment * 0.5));
         //console.log("increment: " + increment);
+        //console.log(new Date());
     }
 
     function adjust() {
         $.get('get-current-value.php').then(function(data, status) {
-            if (status != 'success' || data <= 0) {
+            if (status != 'success' || data.value <= 0 || data.increment <= 0) {
                 setIncrement(defaultIncrement);
                 return;
             }
-            var newValue = parseFloat(data);
-            var deltaValue = newValue - value;
+            defaultIncrement = parseFloat(data.increment);
+            var newValue = parseFloat(data.value);
+            var deltaValue = newValue - currentValue;
             setIncrement(increment + 2000 * deltaValue / adjustInterval);
         });
     }
 
     (function start() {
         $.get('get-current-value.php').then(function(data, status) {
-            if (status != 'success' || data <= 0)
+            if (status != 'success' || data.value <= 0 || data.increment <= 0)
                 return setTimeout(start, 5000);
 
             setup(data);
