96915f297f79564db8cf33fcf79ee7cb993c77da
Charles Ferreira <charles.dev@live.com>
2017-08-15 17:15:07 -0300

    Alteração para tentativa de correção de bug por perda de sincronia. Agora, caso o valor esteja defasado ao consultar o servidor, corrige instantaneamente para o novo valor.

--- js/devolutometro.js
+++ js/devolutometro.js
@@ -4,24 +4,20 @@ $(function() {
     var comma;
     var currentValue;
     var defaultIncrement;
-    var maximumIncrement = function() { return defaultIncrement * 1.5 };
-    var mininumIncrement = function() { return defaultIncrement * 0.5 };
-    var increment = 0;
-    var adjustInterval = 60000;
+    var adjustInterval = 300000;
     var numDigits = 12;
 
     function setup(data) {
         version = data.version;
         currentValue = parseFloat(data.value);
         defaultIncrement = parseFloat(data.increment);
-        setIncrement(defaultIncrement);
         for (var i = 0; i<= numDigits; i++)
             numbers[i] = $('#n' + i);
         comma = $('#virgula');
     }
 
     function update() {
-        currentValue += increment;
+        currentValue += defaultIncrement;
         var digits = currentValue.toFixed(2).replace(/\D/g, '');
         for (var i in numbers) {
             var digit = parseInt(digits[digits.length - 1 - i]);
@@ -34,14 +30,6 @@ $(function() {
         comma.css({ display: currentValue > 1000000000 ? 'block' : 'none' });
     }
 
-    function setIncrement(newValue) {
-        increment = Math.max(mininumIncrement(), newValue > 0
-            ? Math.min(maximumIncrement(), newValue)
-            : Math.min(defaultIncrement, increment * 0.5));
-        //console.log("increment: " + increment);
-        //console.log(new Date());
-    }
-
     function adjust() {
         $.get('get-current-data.php').then(function(data, status) {
             if (data.version != version) {
@@ -49,13 +37,12 @@ $(function() {
                 return;
             }
             if (status != 'success' || data.value <= 0 || data.increment <= 0) {
-                setIncrement(defaultIncrement);
                 return;
             }
             defaultIncrement = parseFloat(data.increment);
             var newValue = parseFloat(data.value);
-            var deltaValue = newValue - currentValue;
-            setIncrement(increment + 2000 * deltaValue / adjustInterval);
+            if (newValue > currentValue)
+                currentValue = newValue;
         });
     }
 
@@ -64,7 +51,7 @@ $(function() {
             if (status != 'success' || data.value <= 0 || data.increment <= 0)
                 return setTimeout(start, 5000);
             setup(data);
-            setInterval(update, 1000);
+            setInterval(update, 1020);
             setInterval(adjust, adjustInterval);
         });
     })();
